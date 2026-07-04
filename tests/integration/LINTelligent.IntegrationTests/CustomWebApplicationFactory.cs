73690c65a3cd94bdf330f8293d7fb5d88d2963e9
Mazen Aly <mazenaly256@gmail.com>
2026-07-04 18:55:43 +0300

    feat: finish integration testing setup and add some integration tests

---
+++ tests/integration/LINTelligent.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,43 @@
+﻿using Hangfire;
+using LINTelligent.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LINTelligent.IntegrationTests;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    private static readonly InMemoryDatabaseRoot DatabaseRoot = new();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            // Remove real PostgreSQL DbContextOptions
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+
+            if (descriptor != null)
+                services.Remove(descriptor);
+
+            // Add EF InMemory
+            services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("TestDb", DatabaseRoot)
+                .UseInternalServiceProvider(new ServiceCollection()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider()));
+
+            // Remove real Hangfire PostgreSQL storage
+            var hangfireDescriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(JobStorage));
+
+            if (hangfireDescriptor != null)
+                services.Remove(hangfireDescriptor);
+
+            services.AddHangfire(config => config.UseInMemoryStorage());
+        });
+    }
+}
---
+++ tests/integration/LINTelligent.IntegrationTests/ReviewsTests.cs
@@ -0,0 +1,96 @@
+﻿using FluentAssertions;
+using LINTelligent.Presentation.DTOs.Request;
+using System.Net;
+using System.Net.Http.Json;
+using System.Reflection.PortableExecutable;
+
+namespace LINTelligent.IntegrationTests;
+
+public class ReviewsTests : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly HttpClient _httpClient;
+
+    public ReviewsTests(CustomWebApplicationFactory factory)
+    {
+        // the client by which I will communicate with the running application instance during testing
+        // It has the application base url by default
+        _httpClient = factory.CreateClient();
+    }
+
+
+    [Fact]
+    public async Task ReviewCodeSnippetAsync_WhenValidInputData_Returns202Accepted()
+    {
+        // Arrange
+        var request = new CodeReviewRequestDto()
+        {
+            CodeSnippet = "console.log('hello')",
+            Language = "JS"
+        };
+
+
+        // Act
+        var response = await _httpClient.PostAsJsonAsync("/reviews", request);
+
+
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Accepted);
+        response.Headers.Location.Should().NotBeNull();
+    }
+
+
+    [Fact]
+    public async Task ReviewCodeSnippetAsync_WhenInvalidInputData_Returns400BadRequest()
+    {
+        // Arrange
+        var request = new CodeReviewRequestDto()        // Language is considered as mandatory, due to ApiController attribute
+        {
+            CodeSnippet = "console.log('hello')"
+        };
+
+
+        // Act
+        var response = await _httpClient.PostAsJsonAsync("/reviews", request);
+
+
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+
+    [Fact]
+    public async Task GetReviewByIdAsync_WhenReviewIdExists_Returns200Ok()
+    {
+        // Arrange
+        var request = new CodeReviewRequestDto()        // Language is considered as mandatory, due to ApiController attribute auto-validates the model (ApiController filter)
+        {
+            CodeSnippet = "console.log('hello')",
+            Language = "JS"
+        };
+
+        var newReviewLocation = (await _httpClient.PostAsJsonAsync("/reviews", request)).Headers.Location;
+
+        // Act
+        var response = await _httpClient.GetAsync(newReviewLocation);
+
+
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+
+    [Fact]
+    public async Task GetReviewByIdAsync_WhenReviewIdDoesNotExist_Returns404NotFound()
+    {
+        // Arrange
+        
+
+
+        // Act
+        var response = await _httpClient.GetAsync($"/reviews/{Guid.NewGuid()}");
+
+
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+}
