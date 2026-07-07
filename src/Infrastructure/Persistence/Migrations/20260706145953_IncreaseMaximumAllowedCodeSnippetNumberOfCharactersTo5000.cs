a2df95b1454b6dc5efa166cadf21dec555785c7a
Mazen Aly <mazenaly256@gmail.com>
2026-07-07 11:33:40 +0300

    feat: add migration that increases allowed characters to 5000 column of database constraints

---
+++ src/Infrastructure/Persistence/Migrations/20260706145953_IncreaseMaximumAllowedCodeSnippetNumberOfCharactersTo5000.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LINTelligent.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreaseMaximumAllowedCodeSnippetNumberOfCharactersTo5000 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CodeSnippet",
+                table: "Reviews",
+                type: "character varying(5000)",
+                maxLength: 5000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CodeSnippet",
+                table: "Reviews",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(5000)",
+                oldMaxLength: 5000);
+        }
+    }
+}
