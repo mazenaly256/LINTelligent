e96ce93ca7090b67184abd897bfa65341986db49
Mazen Aly <mazenaly256@gmail.com>
2026-07-12 09:17:13 +0300

    feat: finish the basic setup for MCP server

--- LINTelligent.McpServer/src/Program.cs
+++ LINTelligent.McpServer/src/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);  // To initialize the DI container
+
+builder.Services.AddHttpClient();   // To register IHttpClientFactory in DI container
+
+builder.Services.AddMcpServer()     // To register the required dependencies to make the app act as MCP server
+    .WithStdioServerTransport()     // To communicate with stdin and stdout
+    .WithToolsFromAssembly();   // To indicate where the tools (available methods) to call are, those that decorated with [McpServerTool] attribute
+
+var app = builder.Build();
+
+await app.RunAsync();   // To block the execution and keep the application process running
